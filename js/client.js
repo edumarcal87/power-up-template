646147cb6a2ffbf9f03b7d0e57bf00601a820437
edumarcal87 <edumarcal87@gmail.com>
2017-12-27 16:31:52 -0200

    Update client.js

--- js/client.js
+++ js/client.js
@@ -139,13 +139,50 @@ var getBadges = function(t){
 
 var boardButtonCallback = function(t){
   return t.popup({
-    title: 'Popup List Example',
+    title: 'Qual o tipo de demanda?',
     items: [
       {
-        text: 'Open Modal',
+        text: 'Demanda de tagueamento',
         callback: function(t){
           return t.modal({            
             url: './modal.html', // The URL to load for the iframe
+            args: { text: 'Hello' }, // Optional args to access later with t.arg('text') on './modal.html'
+            accentColor: '#F2D600', // Optional color for the modal header 
+            height: 500, // Initial height for iframe; not used if fullscreen is true
+            fullscreen: true, // Whether the modal should stretch to take up the whole screen
+            callback: () => console.log('Goodbye.'), // optional function called if user closes modal (via `X` or escape)
+            title: 'Hello, Modal!', // Optional title for modal header
+            // You can add up to 3 action buttons on the modal header - max 1 on the right side.
+            actions: [{
+              icon: GRAY_ICON,
+              url: 'https://google.com', // Opens the URL passed to it.
+              alt: 'Leftmost',
+              position: 'left',
+            }
+            , {
+              icon: GRAY_ICON,
+              callback: (tr) => tr.popup({ // Callback to be called when user clicks the action button.
+                title: 'Settings',
+                url: 'settings.html',
+                height: 164,
+              }),
+              alt: 'Second from left',
+              position: 'left',
+            }, {
+              icon: GRAY_ICON,
+              callback: () => console.log('🏎'),
+              alt: 'Right side',
+              position: 'right',
+            }
+            ],
+          })
+        }
+      }
+      {
+        text: 'Demanda de Teste A/B/N',
+        callback: function(t){
+          return t.modal({            
+            url: './modal-abn.html', // The URL to load for the iframe
             args: { text: 'Hello' }, // Optional args to access later with t.arg('text') on './modal.html'
             accentColor: '#F2D600', // Optional color for the modal header 
             height: 500, // Initial height for iframe; not used if fullscreen is true
@@ -319,7 +356,7 @@ TrelloPowerUp.initialize({
       // we can either provide a button that has a callback function
       // that callback function should probably open a popup, overlay, or boardBar
       icon: WHITE_ICON,
-      text: 'Popup',
+      text: 'Abrir Nova Demanda',
       callback: boardButtonCallback
     }
     // , {
