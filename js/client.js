2f7748afe76dc002ef514708f12cd1331f45d512
edumarcal87 <edumarcal87@gmail.com>
2017-12-27 17:17:00 -0200

    Update client.js

--- js/client.js
+++ js/client.js
@@ -84,58 +84,58 @@ var randomBadgeColor = function() {
   return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
 };
 
-var getBadges = function(t){
-  return t.card('name')
-  .get('name')
-  .then(function(cardName){
-    console.log('We just loaded the card name for fun: ' + cardName);
+// var getBadges = function(t){
+//   return t.card('name')
+//   .get('name')
+//   .then(function(cardName){
+//     console.log('We just loaded the card name for fun: ' + cardName);
     
-    return [{
-      // dynamic badges can have their function rerun after a set number
-      // of seconds defined by refresh. Minimum of 10 seconds.
-      dynamic: function(){
-        // we could also return a Promise that resolves to this as well if we needed to do something async first
-        return {
-          title: 'Detail Badge', // for detail badges only
-          text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-          icon: GRAY_ICON, // for card front badges only
-          color: randomBadgeColor(),
-          refresh: 10 // in seconds
-        };
-      }
-    }, {
-      // its best to use static badges unless you need your badges to refresh
-      // you can mix and match between static and dynamic
-      title: 'Detail Badge', // for detail badges only
-      text: 'Static',
-      icon: GRAY_ICON, // for card front badges only
-      color: null
-    }, {
-      // card detail badges (those that appear on the back of cards)
-      // also support callback functions so that you can open for example
-      // open a popup on click
-      title: 'Popup Detail Badge', // for detail badges only
-      text: 'Popup',
-      icon: GRAY_ICON, // for card front badges only
-      callback: function(context) { // function to run on click
-        return context.popup({
-          title: 'Card Detail Badge Popup',
-          url: './settings.html',
-          height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
-        });
-      }
-    }, {
-      // or for simpler use cases you can also provide a url
-      // when the user clicks on the card detail badge they will
-      // go to a new tab at that url
-      title: 'URL Detail Badge', // for detail badges only
-      text: 'URL',
-      icon: GRAY_ICON, // for card front badges only
-      url: 'https://trello.com/home',
-      target: 'Trello Landing Page' // optional target for above url
-    }];
-  });
-};
+//     return [{
+//       // dynamic badges can have their function rerun after a set number
+//       // of seconds defined by refresh. Minimum of 10 seconds.
+//       dynamic: function(){
+//         // we could also return a Promise that resolves to this as well if we needed to do something async first
+//         return {
+//           title: 'Detail Badge', // for detail badges only
+//           text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+//           icon: GRAY_ICON, // for card front badges only
+//           color: randomBadgeColor(),
+//           refresh: 10 // in seconds
+//         };
+//       }
+//     }, {
+//       // its best to use static badges unless you need your badges to refresh
+//       // you can mix and match between static and dynamic
+//       title: 'Detail Badge', // for detail badges only
+//       text: 'Static',
+//       icon: GRAY_ICON, // for card front badges only
+//       color: null
+//     }, {
+//       // card detail badges (those that appear on the back of cards)
+//       // also support callback functions so that you can open for example
+//       // open a popup on click
+//       title: 'Popup Detail Badge', // for detail badges only
+//       text: 'Popup',
+//       icon: GRAY_ICON, // for card front badges only
+//       callback: function(context) { // function to run on click
+//         return context.popup({
+//           title: 'Card Detail Badge Popup',
+//           url: './settings.html',
+//           height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
+//         });
+//       }
+//     }, {
+//       // or for simpler use cases you can also provide a url
+//       // when the user clicks on the card detail badge they will
+//       // go to a new tab at that url
+//       title: 'URL Detail Badge', // for detail badges only
+//       text: 'URL',
+//       icon: GRAY_ICON, // for card front badges only
+//       url: 'https://trello.com/home',
+//       target: 'Trello Landing Page' // optional target for above url
+//     }];
+//   });
+// };
 
 var boardButtonCallback = function(t){
   return t.popup({
@@ -328,7 +328,7 @@ TrelloPowerUp.initialize({
     ];
   },
   'card-badges': function(t, options){
-    return getBadges(t);
+    // return getBadges(t);
   },
   'card-buttons': function(t, options) {
     return [{
@@ -346,7 +346,7 @@ TrelloPowerUp.initialize({
     }];
   },
   'card-detail-badges': function(t, options) {
-    return getBadges(t);
+    // return getBadges(t);
   },
   'card-from-url': function(t, options) {
     // options.url has the url in question
